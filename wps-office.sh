305266ba12a21729da2710cba7f519a7ef917af8
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-08-19 02:42:17 +0200

    Update wps-office.sh

--- wps-office.sh
+++ wps-office.sh
@@ -12,7 +12,7 @@ if test -f ./appimagetool; then
 	echo " appimagetool already exists" 1> /dev/null
 else
 	echo " Downloading appimagetool..."
-	wget -q "$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')" -O appimagetool
+	wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
 fi
 if test -f ./pkg2appimage; then
 	echo " pkg2appimage already exists" 1> /dev/null
@@ -153,6 +153,6 @@ rsync -av ./mui/* ./$APP/$APP.AppDir/opt/kingsoft/wps-office/office6/mui/
 rm -f -R ./*.7z
 
 # EXPORT THE APP TO AN APPIMAGE
-ARCH=x86_64 VERSION=$(./appimagetool -v | grep -o '[[:digit:]]*') ./appimagetool -s ./$APP/$APP.AppDir
+ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 ./$APP/$APP.AppDir
 cd ..
 mv ./tmp/*.AppImage ./WPS-Office_$VERSION-x86_64.AppImage
